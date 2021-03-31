2f0cbf9a5b3f1af9caf901ea78ad07806e6e98ed
Julian Stobbe <Atomtomate@gmx.de>
2021-03-31 13:20:02 +0200

    Documenter

---
+++ docs/make.jl
@@ -0,0 +1,26 @@
+using SeriesAcceleration
+using Documenter
+
+push!(LOAD_PATH, "../src/")
+makedocs(;
+    modules=[EquivalenceClassesConstructor],
+    authors="Julian Stobbe <Atomtomate@gmx.de> and contributors",
+    repo="https://github.com/Atomtomate/SeriesAcceleration.jl/blob/{commit}{path}#L{line}",
+    sitename="Series Acceleration",
+    format=Documenter.HTML(;
+        prettyurls=get(ENV, "CI", nothing) == "true",
+        canonical="https://Atomtomate.github.io/SeriesAcceleration.jl",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    branch="gh-pages",
+    devbranch = "master",
+    devurl = "stable",
+    repo="github.com/Atomtomate/SeriesAcceleration.jl.git",
+)
+
